d7cd37d33e30691d13d2a484bff3f862f07325bf
ostia <ostia@DESKTOP-7IT16JQ>
2023-06-06 23:42:06 +0900

    1

---
+++ ezimuth.m
@@ -0,0 +1,18 @@
+function az = azimuth(ENU)
+   
+    [n, ~] = size(ENU);
+
+    az = zeros(1, n);
+    
+    for i = 1:n
+        E = ENU(i, 1);
+        N = ENU(i, 2);
+        U = ENU(i, 3);
+       
+        az(i) = atan2(E, N) * 180 / pi;
+
+        if az(i) < 0
+            az(i) = az(i) + 360;
+        end
+    end
+end
